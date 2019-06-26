9421652e1240554da084c5a784bdfce12a9503e6
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-06-26 09:04:29 -0700

    Dynamic Burger Ingredients

---
+++ burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './BuildControls.css';
+
+const buildControls = (props) => {
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default buildControls;
--- burger-builder-app/src/components/Burger/Burger.js
+++ burger-builder-app/src/components/Burger/Burger.js
@@ -4,13 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => { 
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, [] );
 
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add Ingredients</p>;
+    }
 
     console.log(transformedIngredients);
 
