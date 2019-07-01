e0757dce9b5d2cd29e448112c6c46c7cea7381b9
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-07-01 06:52:25 -0700

    adding modal component

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Auxiliary';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENTS_PRICES = {
     salad: 0.5, 
@@ -84,6 +86,9 @@ class BurgerBuilder extends Component {
 
         return(
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger 
                     ingredients={this.state.ingredients} />
                 <BuildControls 
