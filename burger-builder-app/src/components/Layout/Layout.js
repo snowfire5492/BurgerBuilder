7c0a82fbda5ac1e91ddf8cfed3ad782bb77e4731
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-07-02 00:47:32 -0700

    adding a Toolbar component

--- burger-builder-app/src/components/Layout/Layout.js
+++ burger-builder-app/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 
 const layout = (props) => ( // essentially a return statement
     <Aux>
-        <div>Toolbar, SideDrawer, BackDrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
