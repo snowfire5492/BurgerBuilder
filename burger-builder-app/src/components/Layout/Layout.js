fcf0a4363cd0b3d5023dc8c6a28d4b4a0beb5167
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-08-01 19:17:56 -0700

    update backdrop

--- burger-builder-app/src/components/Layout/Layout.js
+++ burger-builder-app/src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
-const layout = (props) => ( // essentially a return statement
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
 
-export default layout;
+class Layout extends Component {  // essentially a return statement
+
+    state = {
+        showSideDrawer : true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer : false });
+    }
+
+    render(){
+        return (
+        <Aux>
+            <Toolbar />
+            <SideDrawer 
+                open={this.state.showSideDrawer}
+                closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        )
+    }
+   
+}
+export default Layout;
