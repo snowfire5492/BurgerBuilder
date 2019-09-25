b2ca6e84029892c449e3371cbfbb9d83d50eb96c
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-09-25 03:19:01 -0700

    adding order

--- burger-builder-app/src/containers/Checkout/Checkout.js
+++ burger-builder-app/src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import CheckoutSummary from '../../components/Orders/CheckoutSummary/CheckoutSummary';
 import { Route } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
 
