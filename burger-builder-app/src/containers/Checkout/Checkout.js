c54d921e7a4db4ce6a382d3e185e55b489feb410
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-09-24 22:55:09 -0700

    fix Checkout.js

--- burger-builder-app/src/containers/Checkout/Checkout.js
+++ burger-builder-app/src/containers/Checkout/Checkout.js
@@ -41,7 +41,8 @@ class Checkout extends Component {
                 checkoutContinued={this.checkoutContinuedHandler}/>
                 <Route path={this.props.match.path + '/contact-data'}
                         render={() => (<ContactData ingredients={this.state.ingredients}/>)} 
-                        price={this.state.totalPrice} />              
+                        price={this.state.totalPrice} 
+                        {...this.checkoutContinuedHandlerprops} />              
             </div>
         )
     }
