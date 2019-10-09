71a8716b0e20bf0ac78c2d3dc90338e211274f3b
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-10-09 00:33:39 -0700

    fixing validation rules

--- burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required:  true
+                },
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required:  true
+                },
+                valid: false
             },
             postal: {
                 elementType: 'input',
@@ -30,7 +38,13 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Postal'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required:  true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -38,7 +52,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required:  true
+                },
+                valid: false
             },
             email: {
                 elementConfig: 'input',
@@ -46,7 +64,11 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'Email'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required:  true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -89,6 +111,23 @@ class ContactData extends Component {
 
     }
 
+    checkValidity(value, rules) {
+
+        let isValid = true;
+
+        if (rules.required){
+            isValid = value.trim() !== '' && isValid;
+        }
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid; 
+        }
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        return isValid
+    }
+
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm // cloning orderForm
@@ -97,6 +136,9 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier] // deeply cloning element
         };
         updatedFormElement.value = event.target.value;  //appending new value to element
+        updatedFormElement.valid = this.checkValidity(
+                updatedFormElement.value, updatedFormElement.validation);
+        console.log(updatedFormElement);
         updatedOrderForm[inputIdentifier] = updatedFormElement; // adding element to orderform
         this.setState({orderForm: updatedOrderForm});   // setting state
     }
