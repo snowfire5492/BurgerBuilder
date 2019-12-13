76d7dbac22a5b287f991a85b6bdaebde84b03b60
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-12-12 23:54:51 -0800

    setup action creators for order submission

--- burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
@@ -111,14 +111,7 @@ class ContactData extends Component {
             orderData: formData
         }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false });
-                this.props.history.push('/');
-            })
-            .catch( error => {
-                this.setState({ loading: false });
-            });
+        
 
     }
 
