d8f01bd95071b45a5afed1fa6b82606060da66a7
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2019-12-13 18:32:55 -0800

    making spinner work thru redux

--- burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
@@ -186,7 +186,7 @@ class ContactData extends Component {
             </form>
         )
 
-        if(this.state.loading) {
+        if(this.props.loading) {
             form = <Spinner />; 
         }
 
@@ -203,13 +203,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     }
 }
 
