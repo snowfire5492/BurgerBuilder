2e6785593ef81d6d0b100bb7466017a558468793
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-04-23 23:11:20 -0700

    logging users out timeout

--- burger-builder-app/src/store/actions/auth.js
+++ burger-builder-app/src/store/actions/auth.js
@@ -23,6 +23,21 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+} 
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    }
+}
+
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
@@ -43,6 +58,7 @@ export const auth = (email, password, isSignup) => {
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.userId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
                
