76a7bcfdf043feb4d759b03139051ba3338338b8
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-04-23 22:07:32 -0700

    storing the token

--- burger-builder-app/src/index.js
+++ burger-builder-app/src/index.js
@@ -7,12 +7,19 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import authReducer from './store/reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
 
-const store = createStore(burgerBuilderReducer,
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    auth: authReducer
+});
+
+
+const store = createStore(rootReducer,
     composeEnhancers( applyMiddleware(thunk)));
 
 const app = (
