89c04cbe20f5838cf297a6eafdfa845b25b902b5
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-04-23 17:42:09 -0700

    Getting Auth Token from backend

--- burger-builder-app/src/store/actions/auth.js
+++ burger-builder-app/src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,22 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+
+
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCtiBPe-WLA-eUvw5ZS5p31x0625bFLTLU', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };
