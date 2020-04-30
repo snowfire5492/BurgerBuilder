ddaaf89126f98033706a7ba865511c177477bd5a
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-04-30 00:21:56 -0700

    get user selected data

--- burger-builder-app/src/store/actions/order.js
+++ burger-builder-app/src/store/actions/order.js
@@ -65,7 +65,10 @@ export const fetchOrdersStart = () => {
 export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
+
+       
         const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+       
         axios.get( '/orders.json' + queryParams)
             .then( res => {
                 const fetchedOrders = [];
